47c07adf5da58471da5f38e3bc495821251a85f7
Tri Le <tri.le@ninjavan.co>
2020-12-11 16:07:21 +0700

    Bump

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,44 @@
 const path = require('path')
-const { readJSONSync } = require('fs-extra')
+const convict = require('convict')
 
-module.exports.read = (filename = 'i18n.config.json') => readJSONSync(
-  path.resolve(process.cwd(), filename)
-)
+convict.addFormat({
+  name: 'array-string',
+  validate: (v) => Array.isArray(v) && !v.find(x => !typeof x === 'string')
+})
+
+const config = convict({
+  lokaliseProjectId: {
+    format: String,
+    default: null,
+  },
+  lokaliseTokenEnvName: {
+    format: String,
+    default: 'LOKALISE_TOKEN',
+  },
+  downloadDestinationPath: {
+    format: String,
+    default: null,
+  },
+  uploadSourcePath: {
+    format: String,
+    default: '',
+  },
+  extractOutputFile: {
+    format: String,
+    default: null,
+  },
+  extractPaths: {
+    format: 'array-string',
+    default: ['./src']
+  },
+  ignoredPaths: {
+    format: 'array-string',
+    default: ['./**/tests']
+  }
+})
+
+module.exports.read = (filename = 'i18n.config.json') => {
+  const file = path.resolve(process.cwd(), filename)
+  config.loadFile(file).validate()
+  return config.getProperties()
+}
